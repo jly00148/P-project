35311a36f02347f48f3a5bf057b3e1ceca3deb9a
jly <453825818@qq.com>
2020-03-10 21:51:15 +0800

    loading status

--- kmall/admin/src/app.js
+++ kmall/admin/src/app.js
@@ -1,14 +1,19 @@
 import React,{ Component } from 'react';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
 import './app.css';
 import Login from './pages/login/index.js';
+import Home from './pages/Home/index.js';
 
 class App extends Component{
 
     render(){
         return(
+            <Router>
                 <div className='App'>
-                    <Login />
+                <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
                 </div>
+             </Router>
         )
     }
 
--- kmall/admin/src/pages/login/index.js
+++ kmall/admin/src/pages/login/index.js
@@ -1,14 +1,48 @@
 import React,{ Component } from 'react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, message,loading } from 'antd';
+import axios from 'axios';
 import './index.css';
 
 // class NormalLoginForm extends React.Component {
 class NormalLoginForm extends Component {
-    handleSubmit(e) {
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoading:false
+
+        }
+    }
+    handleSubmit(e){
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
         if (!err) {
-            console.log('Received values of form: ', values);
+            // console.log('Received values of form: ', values);
+            this.setState(()=>({
+                isLoading:true
+            }))
+
+            axios({
+                method:'post',
+                url:'http://127.0.0.1:3000/admin/login',
+                data:values
+            })
+            .then(result=>{
+                // console.log(data);
+                if(result.data.code == 0){
+                    window.location.href = '/';
+                }else if(result.data.code == 1){
+                    message.error(result.data.message);
+                }
+            })
+            .catch(err=>{
+                // console.log(err);// 服务器连接失败
+                message.error('网络连接失败，请稍后再试！');//端口错误
+            })
+            .finally(()=>{
+                this.setState(()=>({
+                    isLoading:false
+                }))
+            })
         }
         });
   };
@@ -26,7 +60,8 @@ class NormalLoginForm extends Component {
             {/* 用户名输入框 */}
                 <Form.Item>
                     {getFieldDecorator('username', {
-                        rules: [{ required: true, message: '请输入用户名!' }],
+                        // rules: [{ required: true, message: '请输入用户名!' }],//原代码，required为必需输入项
+                        rules: [{required: true,pattern:/^[a-z0-9_]{3,6}$/i, message: '用户名为3-6位，且为数字和字母以及短下划线!' }],
                     })(
                         <Input
                         prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -38,7 +73,8 @@ class NormalLoginForm extends Component {
             {/* 密码输入框 */}
                 <Form.Item>
                     {getFieldDecorator('password', {
-                        rules: [{ required: true, message: '请输入密码!' }],
+                        // rules: [{ required: true, message: '请输入密码!' }], //原代码
+                        rules: [{ required: true,pattern:/^\w{3,6}$/i, message: '密码为3-6位字符!' }],
                     })(
                         <Input
                         prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -60,7 +96,7 @@ class NormalLoginForm extends Component {
                     </a> */}
                     
                     {/* <Button type="primary" htmlType="submit" className="login-form-button"> */} {/* 登录按钮原代码 */}
-                    <Button type="primary" onClick={this.handleSubmit.bind(this)} className="login-form-button"> {/* 去掉htmlType="submit",加上Form表单上的this.handleSubmit，在上面bind this */}
+                    <Button type="primary" onClick={this.handleSubmit.bind(this)} className="login-form-button" loading={this.state.isLoading}> {/* 去掉htmlType="submit",加上Form表单上的this.handleSubmit，在上面bind this */}
                         登录
                     </Button>
                     {/* Or <a href="">register now!</a> */}
