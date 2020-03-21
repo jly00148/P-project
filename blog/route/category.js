a1ffb9cb020e40d5a72bd68d92d9ba19e73e0a91
jly <453825818@qq.com>
2020-03-21 20:54:23 +0800

    prompt pages

--- blog/route/category.js
+++ blog/route/category.js
@@ -19,35 +19,44 @@ router.get('/',(req,res)=>{
     });
 });
 
+// 显示新增分类页面
 router.get('/add',(req,res)=>{
     res.render('admin/category-add.html',{
         userInfo:req.userInfo
     });
 });
 
+// 向后台添加数据逻辑页面：
 router.post('/add',(req,res)=>{
     const {name,order} = req.body;
     CategorySchema.findOne({name})
     .then(categories=>{
-        if(!categories){
+        if(!categories){ // 新增分类成功
             CategorySchema.insertMany({name,order})
-            .then(categories=>{
-                console.log(categories);
-                res.render('admin/category-add.html',{
-                    userInfo:req.userInfo
+            .then(categories=>{ 
+                res.render('admin/success.html',{
+                    userInfo:req.userInfo,
+                    url:'/category'
                 });
             })
             .catch(err=>{
-                throw err;
+                res.render('admin/error.html',{
+                    userInfo:req.userInfo,
+                    msg:'操作数据库出错，请稍后再试'
+                });
             })
-        }else{
-            res.render('admin/category-add.html',{
+        }else{ // 新增分类失败，数据库里内容已经存在
+            res.render('admin/error.html',{
                 userInfo:req.userInfo,
+                msg:'分类名称已存在，请重新输入!'
             });
         }
     })
     .catch((err)=>{
-        throw err;
+        res.render('admin/error.html',{
+            userInfo:req.userInfo,
+            msg:'操作数据库出错，请稍后再试'
+        });
     })
 
 
