017c865205718f74ab98e16b03b461cf9951ab33
jly <453825818@qq.com>
2020-04-13 23:50:37 +0800

    review

--- kmall/admin/src/app.js
+++ kmall/admin/src/app.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import { BrowserRouter as Router,Route} from 'react-router-dom';
+import { BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
 import './app.css';
 import Login from './pages/login/index.js';
 import Home from './pages/Home/index.js';
--- kmall/admin/webpack.config.js
+++ kmall/admin/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');// 新用法
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// console.log(CleanWebpackPlugin);
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');// 清除多余文件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // css单独打包成一个文件,使页面link引入css,而不是用出口js文件在head标签生成style标签。
+
 const publicPath = "/";
 module.exports = {
     mode:'development',
@@ -11,12 +11,12 @@ module.exports = {
     },
     output:{
         filename:'[name].[hash].bundle.js',
-        publicPath:publicPath,
+        publicPath:publicPath, // 生成的index.js文件从根开始
         path:path.resolve(__dirname,'dist')
     },
     module:{
         rules:[
-            // 旧写法
+            // 处理css旧写法
             // {
             //     test:/\.css$/i,
             //     use:[
@@ -26,6 +26,7 @@ module.exports = {
             // },
 
             {
+                // 处理css新写法
                 test:/\.css$/,
                 use:[
                     {
@@ -79,5 +80,4 @@ module.exports = {
         port:8080,//服务器运行的端口
         historyApiFallback:true // H5路由刷新页面不用向后台发送数据
     }
-
 }
