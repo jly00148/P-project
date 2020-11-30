c967436406c9710749df1919b8b973ae7ed6be69
jly <453825818@qq.com>
2020-11-30 18:08:40 +0800

    swiper

--- kmall/home/src/pages/index/index.js
+++ kmall/home/src/pages/index/index.js
@@ -1,8 +1,10 @@
+import Swiper from 'swiper'; //ES引入
+require('swiper/dist/css/swiper.min.css')
 require('./index.css')
 require('pages/common/search')
 require('pages/common/nav')
 require('pages/common/footer')
-var hogan = require('hogan.js')
+var hogan = require('hogan.js')//commonJs引入
 var api = require('api')
 var tpl = require('./categoriesTpl.tpl')
 var _util = require('util')
@@ -10,7 +12,8 @@ var _util = require('util')
 
 var page = {
     init:function(){
-        this.loadHomeCategory()
+        this.loadHomeCategory(),
+        this.swiper()
     },
     loadHomeCategory:function(){
         api.loadHomeCategory({
@@ -19,8 +22,33 @@ var page = {
                 $('.categories').html(html)
             }
         })
+    },
+    swiper:function(){
+        var mySwiper = new Swiper ('.swiper-container', {
+          autoplay:true,//等同于以下设置
+            autoplay: {
+            delay: 2000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false,
+            },
+            // direction: 'vertical', // 垂直切换选项
+            loop: true, // 循环模式选项
+            
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+              clickable:true
+            },
+            
+            // 如果需要前进后退按钮
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          }) 
     }
 }
+
 $(function(){
     page.init()
 })
