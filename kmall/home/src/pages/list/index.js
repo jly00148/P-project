c9c4e4e7760e91a74266599cf6e0965430a72c83
jly <453825818@qq.com>
2020-12-02 22:20:14 +0800

    modify style

--- kmall/home/src/pages/list/index.js
+++ kmall/home/src/pages/list/index.js
@@ -2,3 +2,23 @@ require('./index.css')
 require('pages/common/nav')
 require('pages/common/search')
 require('pages/common/footer')
+var hogan = require('hogan.js')//commonJs引入
+var api = require('api')
+
+var _util = require('util')
+
+
+var page = {
+    init:function(){
+        this.loadProductList()
+    },
+
+    //轮播图左侧
+    loadProductList:function(){
+
+    },
+}
+
+$(function(){
+    page.init()
+})
