cccd18d59fb1337cc35264a3b7cd9f4318c817cb
jly <453825818@qq.com>
2020-12-10 23:33:14 +0800

    order confirm

--- kmall/home/src/pages/order-confirm/modal.js
+++ kmall/home/src/pages/order-confirm/modal.js
@@ -1,7 +1,7 @@
 var api = require('api')
 var hogan = require('hogan.js')
 var _util = require('util')
-var citys = require('util/city');
+var district = require('util/district');
 var modalBoxTpl = require('./modalBox.tpl')
 
 
@@ -10,16 +10,44 @@ module.exports = {
         this.$elem = $('.modal-box')
         this.loadModal()
         this.bindEvent()
+        this.loadProvinceOptions()
 
     },
     loadModal:function(){
-        console.log(citys.getProvinces())
-        console.log(citys.getCities('湖南省'))
+        // citys.getProvinces()
+        // citys.getCities()
         var html = _util.render(modalBoxTpl,{},hogan)
         this.$elem.html(html)
     },
     hideModal:function(){
         this.$elem.empty()
+    },
+    //遍历后的HTML插入到父节点内
+    loadProvinceOptions:function(){
+        var productHtml = this.loadProvince()
+        $('.city-item').find('.province-select').html(productHtml)
+
+    },
+    loadCitysOptions:function(citys){
+        $('.city-item').find('.city-select').html(citys)
+    },
+    //遍历省份
+    loadProvince:function(){
+        var _provinces = district.getProvinces()
+        var html = '<option value="">请选择</option>'//第一行默认
+        for(var i = 0;i<_provinces.length;i++){
+            html+='<option value="'+_provinces[i]+'">'+_provinces[i]+'</option>'
+        }
+        return html
+    },
+    //遍历省份内的城市
+    loadCitys:function(city){
+        var citys = district.getCities(city)
+        var html = '<option value="">请选择</option>'
+        for(var i = 0;i<citys.length;i++){
+            html+='<option value="'+citys[i]+'">'+citys[i]+'</option>'
+        }
+        return html
     },
     bindEvent:function(){
         var _this = this
@@ -31,6 +59,13 @@ module.exports = {
         //阻止冒泡
         this.$elem.on('click','.modal-container',function(ev){
             ev.stopPropagation()
-        })        
+        }) 
+        
+        //省份和城市联动
+        this.$elem.on('change','.province-select',function(){
+            var $this = $(this)
+            var citys = _this.loadCitys($this.val())
+            _this.loadCitysOptions(citys)//返回遍历成HTML直接插入到父元素中
+        })
     }
 }
